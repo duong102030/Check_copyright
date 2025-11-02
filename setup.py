914970b05d71d44701401a845f35ec72ce18adbc
Nguyen Kha Duong <nguyenkhaduong1010@gmail.com>
2025-11-02 17:59:39 +0700

    fix test-precommit

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     name="check-copyright-nkd",
     version="1.0.0",
     py_modules=["check_copyright"],
+    entry_points={
+        "console_scripts": [
+            "check_copyright = check_copyright:main",
+        ],
+    },
 )
